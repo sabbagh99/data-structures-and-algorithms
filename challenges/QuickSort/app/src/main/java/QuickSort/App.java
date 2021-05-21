56ccfb22d6656ebcfbbffce1ee8144dd8980588b
sabbagh99 <a.sameer1999@gmail.com>
2021-05-21 18:56:11 +0300

    edit code challenge 28

--- challenges/QuickSort/app/src/main/java/QuickSort/App.java
+++ challenges/QuickSort/app/src/main/java/QuickSort/App.java
@@ -9,8 +9,8 @@ public class App {
 
   public static void main(String[] args) {
     QuickSort quickSort = new QuickSort();
-    int[] arr = { 8,4,23,42,16,15 };
-    System.out.println(
-      Arrays.toString(quickSort.QuickSort(arr,0,arr.length-1)));
+    int[] arr = {8,4,23,42,16,15 };
+    System.out.println(Arrays.toString(quickSort.QuickSort(arr,0,arr.length-1)));
+
   }
 }
