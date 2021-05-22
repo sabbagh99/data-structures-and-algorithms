1c429145b3a62f572410f6f59fda349f8c969b23
sabbagh99 <a.sameer1999@gmail.com>
2021-05-22 19:46:11 +0300

    fix code challenge 27

--- challenges/MergeSort/app/src/main/java/MergeSort/MergeSort.java
+++ challenges/MergeSort/app/src/main/java/MergeSort/MergeSort.java
@@ -6,12 +6,12 @@ public class MergeSort {
   public int[] mergesort(int[] arr) {
 
 
-  int n = arr.length;
+    int n = arr.length;
 
-      if( n >1) {
-      int mid=n / 2;
+    if (n > 1) {
+      int mid = n / 2;
       int[] left = new int[mid];
-      int[] right = new int[n-mid];
+      int[] right = new int[n - mid];
 
       for (int i = 0; i < left.length; i++)
         left[i] = arr[i];
@@ -29,26 +29,74 @@ public class MergeSort {
       merge(left, right, arr);
     }
     return arr;
-}
-  public void merge(int []left, int []right, int []arr) {
-    int i =0;
-    int j =0;
-    int k =0;
+  }
 
-    while (i<left.length &&j<right.length) {
+  public void merge(int[] left, int[] right, int[] arr) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+//    int count = 0;
+
+    while (i < left.length && j < right.length) {
       if (left[i] <= right[j]) {
-        arr[k] =left[i];
-        i=i + 1;
+        arr[k] = left[i];
+        i++;
       } else {
-        arr[k] =right[j];
-        j=j + 1;
-      }
-      k=k + 1;
-
-      if (i == left.length)
         arr[k] = right[j];
-            else
-              arr[k] = left[i];
+        j++;
+      }
+      k++;
+//    }
+//      if (i == left.length) {
+//        count = j;
+//        while (count < right.length) {
+//          arr[k] = right[count];
+//          count++;
+//          k++;
+//        }
+//        count = 0;
+//      } else {
+//        count = i;
+//        while (count < left.length) {
+//          arr[k] = left[count];
+//          count++;
+//          k++;
+//
+//        }
+//      }
+//      count = 0;
+
     }
+
+
+
+      if (i == left.length) {
+        while(j<right.length) {
+          arr[k] = right[j];
+          k++;
+          j++;
+        }
+      }else {
+        while(i<left.length) {
+          arr[k] = left[i];
+          k++;
+          i++;
+        }
+      }
+
+
+
   }
 }
+
+//while (i<left.length){
+//  arr[k]= left[i];
+//  k++;
+//  i++;
+//  }
+//
+//  while (j<right.length){
+//  arr[k]= right[j];
+//  k++;
+//  j++;
+//  }
