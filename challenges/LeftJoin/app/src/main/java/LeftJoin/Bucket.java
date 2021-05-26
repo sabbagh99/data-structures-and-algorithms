dd2c6717321fb1fb10c27b483b92743788999171
sabbagh99 <a.sameer1999@gmail.com>
2021-05-26 07:37:09 +0300

    add code challenge 33

---
+++ challenges/LeftJoin/app/src/main/java/LeftJoin/Bucket.java
@@ -0,0 +1,26 @@
+package LeftJoin;
+
+public class Bucket<T> {
+  T key;
+  T value;
+  Bucket next;
+
+  Bucket(T key, T value){
+    this.key = key;
+    this.value = value;
+    next=null;
+
+  }
+
+  public Bucket() {
+  }
+
+  @Override
+  public String toString() {
+    return "Bucket{" +
+      "key=" + key +
+      ", value=" + value +
+      ", next=" + next +
+      '}';
+  }
+}
